d9ccb4833a9b16917b7cedc2edf325a9aec561d5
alex-raw <alexanderrauhutjs@gmail.com>
2020-12-03 16:53:44 +0100

    added R script for joining frequency list, and exploratory analysis

--- advent1a.R
+++
@@ -1,2 +0,0 @@
-x <- c(scan("day1_data", quiet = TRUE))
-prod(x[match(x, 2020 - x, nomatch = 0)])
--- advent1b.R
+++
@@ -1,8 +0,0 @@
-x <- c(scan("day1_data", quiet = TRUE))
-
-matches <- list()
-for (i in 2020 - x) {
-  matches[[i]] <- x[match(x, i - x, nomatch = 0)]
-}
-
-prod(unique(unlist(matches)))
