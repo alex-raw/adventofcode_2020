f559aaa0057db9cecc95a12d49ff42d922e3b440
alex-raw <alexanderrauhutjs@gmail.com>
2020-12-06 19:28:53 +0100

    finished day 4 and 5

---
+++ aoc_5.R
@@ -0,0 +1,48 @@
+d <- scan("data/aoc_5", what = character(), quiet = TRUE)
+
+# ids of rows and columns
+rw <- 0:127
+cl <- 0:7
+
+# Row
+F <- function(id) {
+  id <<- seq.int(min(id), floor(median(id)))
+}
+
+B <- function(id) {
+  id <<- seq.int(ceiling(median(id)), max(id))
+}
+
+# Column
+L <- F
+R <- B
+
+# interpret character string as sequence of functions
+decode <- function(code) {
+  funs <- strsplit(code, split = "")
+  for (i in funs[[1]]) {
+    do.call(i, list(id))
+  }
+  return(id)
+}
+
+# Find seat id
+seat_id <- function(code) {
+  # row number
+  id <<- rw
+  funs <- gsub("R|L", "", code)
+  x <- decode(funs)
+
+  # column number
+  id <<- cl
+  funs <- gsub("B|F", "", code)
+  y <- decode(funs)
+
+  (x * 8) + y
+}
+
+seats <- sapply(d, seat_id)
+max(seats)
+
+# Part two
+sum(min(seats):max(seats)) - sum(seats)
