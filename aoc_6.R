f070ac157a3d4f43e8f812155372d4e8f1b08bee
alex-raw <alexanderrauhutjs@gmail.com>
2020-12-09 17:29:15 +0100

    cleaned up 6

--- aoc_6.R
+++ aoc_6.R
@@ -1,15 +1,14 @@
 input <- paste(readLines("data/aoc_6"), collapse = "\n")
-data <- strsplit(input, "\n\n")[[1]]
-data <- gsub("\n", ",", data)
+input <- strsplit(input, "\n\n")[[1]]
 
 # Part one
-d <- gsub(",", "", data)
+d <- gsub("\n", "", input)
 d <- strsplit(d, "")
 
 sum(lengths(sapply(d, unique)))
 
 # Part two
-d_group <- strsplit(data, ",")
+d_group <- strsplit(input, "\n")
 d_people_group <- lapply(d_group, strsplit, "")
 common <- lapply(d_people_group, function(x) Reduce(intersect, x))
 
