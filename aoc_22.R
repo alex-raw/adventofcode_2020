694360431632936446560b93bab682822a7613cf
alex-raw <alexanderrauhutjs@gmail.com>
2021-06-05 19:26:50 +0200

    improvements

--- aoc_22.R
+++ aoc_22.R
@@ -1,32 +1,28 @@
 parse_cards <- function(x) {
-  d <- scan(x, na.strings = c("Player", "1:", "Player", "2:"), quiet = TRUE)
-  matrix(na.omit(d), ncol = 2)
+  d <- scan(x, comment.char = "P") |> matrix(ncol = 2)
+  list(d[, 1], d[, 2])
+}
+
+combat <- function(a, b) {
+  wins <- a > b
+  rbind(a[wins], b[wins])
 }
 
 stash <- function(a, b) {
-  wins <- a > b
-  c(rbind(a[wins], b[wins]))
-}
-
-stash_uneq <- function(a, b) {
-  a_sub <- a[seq_along(b)]
-  a <- c(a[-seq_along(b)], stash(a_sub, b))
-  b <- stash(b, a_sub)
-  list(a, b)
+  nb <- seq_along(b)
+  a1 <- a[nb]; a2 <- a[-nb]
+  list(c(a2, combat(a1, b)), combat(b, a1))
 }
 
 play <- function(m) {
-  a <- m[, 1]; b <- m[, 2]
-  while (length(a) != 0 && length(b) != 0) {
-    if (length(a) > length(b)) {
-      l <- stash_uneq(a, b)
-      a <- l[[1]]; b <- l[[2]]
-    } else {
-      l <- stash_uneq(b, a)
-      a <- l[[2]]; b <- l[[1]]
-    }
+  repeat {
+    n <- lengths(m)
+    if (any(n == 0)) break
+    i <- which.max(n)
+    m <- stash(m[[i]], m[[-i]])
   }
-  if (length(b) == 0) winner <- a else winner <- b
+
+  winner <- m[[i]]
   sum(winner * rev(seq_along(winner)))
 }
 
